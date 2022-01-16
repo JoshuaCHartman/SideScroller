2fc88be660bd6c40cf539ef667461cc765de0550
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-16 00:42:11 -0500

    Jump / Double Jump
    double jump added and activated/deactivated by use of bool hasDoulbeJump

--- SideScroller2Point5D/Assets/Scripts/playerMovement.cs
+++ SideScroller2Point5D/Assets/Scripts/playerMovement.cs
@@ -13,30 +13,32 @@ public class playerMovement : MonoBehaviour
     // access the standard character controller and animator (animator has state machines and logic built in animator window in unity)
     private CharacterController _characterController;
     private Animator _playerAnim;
-   
+
     // direction player is moving, that we will apply other variables and factors to
     private Vector3 _movementDirection;
 
     // variables affecting movement and jumping- these can be modified in the inspector window to achieve desired results
-    [SerializeField] private float _playerSpeed = 10f;
+    [SerializeField] private float _playerWalkSpeed = 12f;
     [SerializeField] private float _gravity = 20f;
-    [SerializeField] private float _playerJump = 10f;
+    [SerializeField] private float _playerJump = 14f;
     // this is a visual check to confirm gravity is being applied
     [SerializeField] private float _verticalVelocity;
+
+    // double jump bool - will allow one extra jump
+    private bool hasDoubleJump = false;
 
     // variables to check if player off 0Z and set to 0Z
     private Vector3 _tempPos;
 
     // flip character if going left or right
     // horizontal input >0 = flip facing right, horizontal input <0 flip facing left
-    // must 
     private float _playerFacingDirection;
-    private bool _isFacingLeft = true;
+
     // local transform to flip the frame and model, NOT the parent player object
-    [SerializeField] private Transform playerAnimatedCharacterTF;
+    [SerializeField] private Transform _playerAnimatedCharacterTF;
 
 
-    
+
     private void Awake()
     {
         // references 
@@ -44,7 +46,20 @@ public class playerMovement : MonoBehaviour
         _characterController = GetComponent<CharacterController>();
         _playerAnim = GetComponent<Animator>();
     }
+    private void FixedUpdate()
+    {
+        // if player is not on 0z, change xz value to 0
 
+        _tempPos = transform.position;
+
+        if (transform.position.z != 0)
+        {
+            _tempPos.z = 0;
+        }
+
+        transform.position = _tempPos;
+
+    }
     // Update is called once per frame
     void Update()
     {
@@ -53,24 +68,30 @@ public class playerMovement : MonoBehaviour
 
     void MovePlayer()
     {
+
         // assign input, and use to determine which direciton player is facing, and rotate accordingly
         _playerFacingDirection = Input.GetAxis("Horizontal"); // GetAxis returns values between -1 & 0 (left) or between 0 & 1 (right)
-        if (_playerFacingDirection > 0 )
+        if (_playerFacingDirection > 0)
         {
             // particular y values are dependent on the desired stance/view of character
-            playerAnimatedCharacterTF.localRotation = Quaternion.Euler(0f, -250, 0f);//-270
+            // a local rotation is applied, not globabl
+            _playerAnimatedCharacterTF.localRotation = Quaternion.Euler(0f, -250, 0f);//-270
         }
 
         if (_playerFacingDirection < 0)
         {
             // particular y values are dependent on the desired stance/view of character
-            playerAnimatedCharacterTF.localRotation = Quaternion.Euler(0f, -110, 0f);//270
+            // a local rotation is applied, not globabl
+            _playerAnimatedCharacterTF.localRotation = Quaternion.Euler(0f, -110, 0f);//270
         }
 
         // take input and make a new vector3, change coordinates to global, multiply by speed factor & time.deltatime
         _movementDirection = new Vector3(_playerFacingDirection, 0f, 0f);
         _movementDirection = transform.TransformDirection(_movementDirection); // change coordinates to global
-        _movementDirection *= _playerSpeed * Time.deltaTime;
+       
+        // make if walk/run/idle
+
+        _movementDirection *= _playerWalkSpeed * Time.deltaTime;
 
         // apply gravity. charactercontroller has no gravity component. test for jump.
         ApplyGravityToPlayer();
@@ -82,20 +103,42 @@ public class playerMovement : MonoBehaviour
     private void ApplyGravityToPlayer()
     { // subtract gravity to vertical velocity, use for y value of movementdirection vector3 (x,y,z)
         _verticalVelocity -= _gravity * Time.deltaTime;
-
-        playerJump();
+        // if jumping, change vertical velocity to jump value
+        SetPlayerJump();
         // change the move direction y to a positive one when jumping 
+
         _movementDirection.y = _verticalVelocity * Time.deltaTime;
     }
 
-    void playerJump()
-    {
-        // check if ground & pressed spacebar, apply playerJump value
 
-        if (_characterController.isGrounded == true && Input.GetKey(KeyCode.Space))
+    void SetPlayerJump()
+    {
+        // check if ground & pressed spacebar, has double jump, apply playerJump value
+
+        if (_characterController.isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            // modify playerJump to find desired effect
-            _verticalVelocity = _playerJump;
+            // set double jump bool to true since on ground
+            hasDoubleJump = true;
+
+            // modify playerJump value in inspector & above to find desired effect
+            ApplyPlayerJump();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && hasDoubleJump == true)
+        {
+            // turn off double jump so not able to spam jumps
+            hasDoubleJump = false;
+            ApplyPlayerJump();
         }
     }
+
+    void ApplyPlayerJump()
+    {
+        // set vertical velocity to playerJump value
+        _verticalVelocity = _playerJump;
+        
+    }
+
+
 }
+
