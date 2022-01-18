ed3974784bf0057af43d70a5bd11e024a6d4935c
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-17 21:27:34 -0500

    Projectile FX
    ProjectileElectric -
    electric shoot fx
    electric hit fx instantiate on collision
    disperse/destroy on collisenenter w/ "Enemy" tag

---
+++ SideScroller2Point5D/Assets/Scripts/attackScripts/MagicProjectile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicProjectile : MonoBehaviour
+{
+    public GameObject _damageEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Instantiate(_damageEffect, transform.position, _damageEffect.transform.rotation );
+            Destroy(gameObject);
+        }
+    }
+}
