e91af0e363b33a7705bb6ebfaf672136e2249d42
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-17 23:00:45 -0500

    Reorganized
    Fixed organization of folders

--- SideScroller2Point5D/Assets/Scripts/attackScripts/MagicProjectile.cs
+++ SideScroller2Point5D/Assets/Scripts/attackScripts/MagicProjectile.cs
@@ -6,12 +6,17 @@ public class MagicProjectile : MonoBehaviour
 {
     public GameObject _damageEffect;
 
+   
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Enemy")
+        if(other.tag == "Enemy")
         {
-            Instantiate(_damageEffect, transform.position, _damageEffect.transform.rotation );
+            Instantiate(_damageEffect, transform.position, _damageEffect.transform.rotation);
             Destroy(gameObject);
         }
     }
 }
+
+    
+
+
